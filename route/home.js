466b4fb77cdffc339521e02279447b77b8913676
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2023-12-10 22:17:54 +0500

    Update home.js

--- route/home.js
+++ route/home.js
@@ -3,18 +3,21 @@ const categoryModel = require("./categoryModel");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
+  try {
     const category = await categoryModel.find({});
     res.status(200).send({
       success: true,
       message: "All Categories List",
       category,
     });
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching categories",
+      error: error.message,
+    });
+  }
 });
 
 module.exports = router;
-
-
