63e8c23ea091363869d1c4126f1188def4e09bcc
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2023-12-11 16:06:05 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const categoryController = require("./route/Category/categoryController");
 const authController = require("./route/Users/authController");
 const connectDB = require('./db.js');
+const cors  = require("cors")
 
 connectDB()
 
 // Middlewares
 const app = express();
 app.use(express.json());
+ app.use(cors())
 
 // Routes
 app.use("/api/v1/category", categoryController);
