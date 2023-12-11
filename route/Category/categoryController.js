62f041bac524fe39141b3bb4b049a40bcf45eba2
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2023-12-11 14:49:39 +0500

    Add files via upload

---
+++ route/Category/categoryController.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const categoryModel = require("./categoryModel");
+const slugify = require("slugify");
+
+const router = express.Router();
+
+
+//get all categories
+router.get("/get-category", async (req, res, next) => {
+    const category = await categoryModel.find({});
+    res.status(200).send({
+      success: true,
+      message: "All Categories List",
+      category,
+    });
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
+});
+
+module.exports = router;
+
+// create category
+router.post("/create-category" , async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(401).send({ message: "Name is required" });
+    }
+    const existingCategory = await categoryModel.findOne({ name });
+    if (existingCategory) {
+      return res.status(200).send({
+        success: true,
+        message: "Category Already Exisits",
+      });
+    }
+    const category = await new categoryModel({
+      name,
+      slug: slugify(name),
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "new category created",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      errro,
+      message: "Errro in Category",
+    });
+  }
+});
+
+//update category
+router.put("/update-category/:id", async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { id } = req.params;
+    const category = await categoryModel.findByIdAndUpdate(
+      id,
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      messsage: "Category Updated Successfully",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while updating category",
+    });
+  }
+});
+
+// single category
+router.get("/single-category/:slug", async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    res.status(200).send({
+      success: true,
+      message: "Get SIngle Category SUccessfully",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error While getting Single Category",
+    });
+  }
+});
+
+//delete category
+router.delete("/delete-category/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await categoryModel.findByIdAndDelete(id);
+    res.status(200).send({
+      success: true,
+      message: "Categry Deleted Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error while deleting category",
+      error,
+    });
+  }
+});
---
+++ route/Users/authController.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const userModel = require("./userModel");
+const  JWT = require( "jsonwebtoken");
+const { comparePassword, hashPassword } = require("./helper");
+
+dotenv.config();
+const dotenv = require("dotenv");
+
+const router = express.Router();
+
+
+
+// register user
+
+router.post("/register", async (req, res) => {
+    try {
+      const { name, email, password, phone, address, answer } = req.body;
+      //validations
+      if (!name) {
+        return res.send({ error: "Name is Required" });
+      }
+      if (!email) {
+        return res.send({ message: "Email is Required" });
+      }
+      if (!password) {
+        return res.send({ message: "Password is Required" });
+      }
+      if (!phone) {
+        return res.send({ message: "Phone no is Required" });
+      }
+      if (!address) {
+        return res.send({ message: "Address is Required" });
+      }
+      if (!answer) {
+        return res.send({ message: "Answer is Required" });
+      }
+      //check user
+      const exisitingUser = await userModel.findOne({ email });
+      //exisiting user
+      if (exisitingUser) {
+        return res.status(200).send({
+          success: false,
+          message: "Already Register please login",
+        });
+      }
+      //register user
+      const hashedPassword = await hashPassword(password);
+      //save
+      const user = await new userModel({
+        name,
+        email,
+        phone,
+        address,
+        password: hashedPassword,
+        answer,
+      }).save();
+  
+      res.status(201).send({
+        success: true,
+        message: "User Register Successfully",
+        user,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        message: "Errro in Registeration",
+        error,
+      });
+    }
+  });
+
+  //POST LOGIN
+router.post("/login", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      //validation
+      if (!email || !password) {
+        return res.status(404).send({
+          success: false,
+          message: "Invalid email or password",
+        });
+      }
+      //check user
+      const user = await userModel.findOne({ email });
+      if (!user) {
+        return res.status(404).send({
+          success: false,
+          message: "Email is not registerd",
+        });
+      }
+      const match = await comparePassword(password, user.password);
+      if (!match) {
+        return res.status(200).send({
+          success: false,
+          message: "Invalid Password",
+        });
+      }
+      //token
+      const token = await JWT.sign({ _id: user._id }, process.env.JWT_Secrete, {
+        expiresIn: "1000d",
+      });
+      res.status(200).send({
+        success: true,
+        message: "login successfully",
+        user: {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          phone: user.phone,
+          address: user.address,
+          role: user.role,
+        },
+        token,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        message: "Error in login",
+        error,
+      });
+    }
+  });
