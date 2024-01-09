8bb503497dbfad195d7cf045b53932f56d800da4
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-01-09 10:08:39 +0500

    Update productController.js

--- route/Products/productController.js
+++ route/Products/productController.js
@@ -74,7 +74,6 @@ router.get("/get-product" , async (req, res) => {
       .find({})
       .populate("category")
       .select("-photo")
-      .limit(12)
       .sort({ createdAt: -1 });
     res.status(200).send({
       success: true,
